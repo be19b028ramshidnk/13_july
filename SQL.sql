86ffad68428d48b563dd637aa52a3f8e11a62bbb
RAMSHID N K <87602158+be19b028ramshidnk@users.noreply.github.com>
2024-05-13 17:30:34 +0530

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,151 @@
+CREATE DATABASE IF NOT EXISTS walmartSales;
+CREATE TABLE IF NOT EXISTS sales(
+	invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
+    branch VARCHAR(5) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    customer_type VARCHAR(30) NOT NULL,
+    gender VARCHAR(30) NOT NULL,
+    product_line VARCHAR(100) NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    tax_pct FLOAT(6,4) NOT NULL,
+    total DECIMAL(12, 4) NOT NULL,
+    date DATETIME NOT NULL,
+    time TIME NOT NULL,
+    payment VARCHAR(15) NOT NULL,
+    cogs DECIMAL(10,2) NOT NULL,
+    gross_margin_pct FLOAT(11,9),
+    gross_income DECIMAL(12, 4),
+    rating FLOAT(2, 1)
+);
+
+-- --------------------------------------------------------------------
+-- ---------------------------- Feature Engineering ------------------------------
+-- --------------------------------------------------------------------
+SELECT
+	time,
+	(CASE
+		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
+        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
+        ELSE "Evening"
+    END) AS time_of_day
+FROM sales;
+/*add a column*/
+ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
+
+/* fill the colum with value*/ 
+UPDATE sales
+SET time_of_day = (
+	CASE
+		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
+        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
+        ELSE "Evening"
+    END
+);
+
+-- Add day_name column
+SELECT
+	date,
+	DAYNAME(date)
+FROM sales;
+ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
+
+UPDATE sales
+SET day_name = DAYNAME(date);
+
+
+-- Add month_name column
+SELECT
+	date,
+	MONTHNAME(date)
+from sales;
+
+ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
+
+UPDATE sales
+SET month_name = MONTHNAME(date);
+
+-- --------------------------------------------------------------------
+-- ---------------------------- Exploratory Data Analysis ------------------------------
+-- --------------------------------------------------------------------
+
+
+-- How many unique cities does the data have and in which city is each branch?
+SELECT 
+	DISTINCT city,
+    branch
+FROM sales;
+
+-- What is the most selling product line
+SELECT
+	SUM(quantity) as qty,
+    product_line
+FROM sales
+GROUP BY product_line
+ORDER BY qty DESC;
+
+-- What is the total revenue by month
+SELECT
+	month_name AS month,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY month_name 
+ORDER BY total_revenue;
+
+-- Fetch each product line and add a column to those product 
+-- line showing "Good", "Bad". Good if its greater than average sales
+
+SELECT 
+	AVG(quantity) AS avg_qnty
+FROM sales;
+
+SELECT
+	product_line,
+	CASE
+		WHEN AVG(quantity) > 5.4995 THEN "Good"
+        ELSE "Bad"
+    END AS remark
+FROM sales
+GROUP BY product_line;
+
+-- Which branch sold more products than average product sold?
+SELECT 
+	branch, 
+    SUM(quantity) AS qnty
+FROM sales
+GROUP BY branch
+HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
+
+-- What is the most common customer type?
+SELECT
+	customer_type,
+	count(*) as count
+FROM sales
+GROUP BY customer_type
+ORDER BY count DESC;
+
+-- Which day of the week has the best average ratings per branch?
+SELECT 
+	day_name,
+	COUNT(day_name) total_sales
+FROM sales
+WHERE branch = "C"
+GROUP BY day_name
+ORDER BY total_sales DESC;
+
+-- Number of sales made in each time of the day per weekday 
+SELECT
+	time_of_day,
+	COUNT(*) AS total_sales
+FROM sales
+WHERE day_name = "Sunday"
+GROUP BY time_of_day 
+ORDER BY total_sales DESC;
+
+-- Which city has the largest tax/VAT percent?
+SELECT
+	city,
+    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
+FROM sales
+GROUP BY city 
+ORDER BY avg_tax_pct DESC;
